d423394fdac017c7231eddfd72298dbf696a3e59
Ines Karlina <nesikarlina344@gmail.com>
2026-01-18 17:28:01 +0700

    change baru

--- main.c
+++ main.c
@@ -3,5 +3,7 @@
 void main() {
     printf("Hello, World!\n");
     printf("This is a simple C program.\n");
+    printf("C11 standard is being used.\n");
+    printf("Goodbye!\n");
     return 0;
 }
